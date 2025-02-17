38996dcfe1b23a879246f7c0ead77a111c8349be
Drilord <diego.gonzalezsilva@gmail.com>
2025-02-17 15:08:30 -0600

    add inicio function & responsive for the carrousel

--- control.js
+++ control.js
@@ -1,8 +1,29 @@
 function uncover(divId){
     console.log(divId);
+    
+    hideMen();
     const element = document.getElementById(divId);
     element.style.display = 'block';
     element.style.color = '#FFFFFF';
     toastr.success('Uncovered!', 'Success');
     
+}
+
+function hideMen(){
+
+    const resto= document.querySelectorAll('.men');
+    console.log(resto);
+    resto.forEach(function(resto){
+        resto.style.display = 'none';
+    });
+
+}
+
+function inicio(){
+    hideMen();
+    const resto= document.querySelectorAll('.ini')
+    console.log(resto);
+    resto.forEach((item) => {
+        item.style.display = 'flex';
+    });
 }
